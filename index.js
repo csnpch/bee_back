5bd89ec724041238d3c7abb07a5afbc5a2c2fbd4
beeyah2002 <beesui2545@gmail.com>
2023-10-30 20:25:13 +0700

    update Project

--- index.js
+++ index.js
@@ -1,81 +1,221 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-
-mongoose.connect(
-    "mongodb://admin:KBGlyi82388@10.104.3.62:27017",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-);  
-
-const Book = mongoose.model("Book", {
-    id: Number,
-    title: String,
-    author: String,
-});
-
+const sqlite3 = require('sqlite3');
 const app = express();
-app.use(bodyParser.json());
+const db = new sqlite3.Database('./Database/Bandtable.sqlite');
+
+app.use(express.json());
+
+db.run(`CREATE TABLE IF NOT EXISTS Band (
+    ID INTEGER PRIMARY KEY,
+    Name TEXT,
+    Genre TEXT,
+    FormationYear TEXT,
+    Member TEXT,
+    Pic TEXT
+
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS Album (
+    ID INTEGER PRIMARY KEY,
+    Albumname TEXT,
+    Year TEXT,
+    Song TEXT,
+    Pic TEXT
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS Bandalbum (
+    ID INTEGER PRIMARY KEY,
+    bandID TEXT,
+    albumID TEXT
+
+   
+)`);
 
 
-app.post("/books", async (req, res) => {
-    try {
-        const book = new Book(req.body);
-        book.id = (await Book.countDocuments()) + 1;
-        await book.save();
-        res.send(book);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+
+app.get('/Band', (req, res) => {
+    db.all('SELECT * FROM Band', (err, rows) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+app.get('/Band/:id', (req, res) => {
+    db.get('SELECT * FROM Band WHERE ID = ?', req.params.id, (err, row) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            if (!row) {
+                res.status(404).send('Band Not found');
+            } else {
+                res.json(row);
+            }
+        }
+    });
+});
+
+app.post('/Band', (req, res) => {
+    const band = req.body;
+    db.run('INSERT INTO Band (Name, Genre, FormationYear, Member, Pic) VALUES (?, ?, ?, ?, ?)', band.Name, band.Genre, 
+    band.FormationYear, band.Member, band.Pic, function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            band.ID = this.lastID;
+            res.send(band);
+        }
+    });
+
+});
+
+app.put('/Band/:id', (req, res) => {
+    const band = req.body;
+    db.run('UPDATE band SET Name = ?, Genre = ?, FormationYear = ? , Member = ?  , Pic = ? WHERE ID = ?', band.Name, band.Genre, 
+    band.FormationYear, band.Member, band.Pic , req.params.id, function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(band);
+        }
+    });
+});
+
+app.delete('/band/:id', (req, res) => {
+    db.run('DELETE FROM band WHERE ID = ?', req.params.id, function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send({});
+        }
+    });
+});
+
+// CRUD สำหรับ Habitat
+app.get('/Album', (req, res) => {
+    db.all('SELECT * FROM Album', (err, rows) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+app.get('/Album/:id', (req, res) => {
+    db.get('SELECT * FROM Album WHERE ID = ?', req.params.id, (err, row) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            if (!row) {
+                res.status(404).send('Album Not found');
+            } else {
+                res.json(row);
+            }
+        }
+    });
+});
+
+app.post('/Album', (req, res) => {
+    const album = req.body;
+    db.run('INSERT INTO Album (Albumname, Year, Song , Pic) VALUES (?, ?, ?, ?)', album.Albumname, 
+    album.Year, album.Song, album.Pic ,function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            album.ID = this.lastID;
+            res.send(album);
+        }
+    });
+});
+
+app.put('/Album/:id', (req, res) => {
+    const album = req.body;
+    db.run('UPDATE album SET Albumname = ?, Year = ?, Song = ?, Pic = ? WHERE ID = ?', album.Albumname, album.Year, 
+    album.Song,album.Pic , req.params.id, function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(album);
+        }
+    });
+});
+
+app.delete('/Album/:id', (req, res) => {
+    db.run('DELETE FROM Album WHERE ID = ?', req.params.id, function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send({});
+        }
+    });
+});
+
+app.get('/Bandalbum', (req, res) => {
+    db.all('SELECT * FROM Bandalbum', (err, rows) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+app.get('/Bandalbum/:id', (req, res) => {
+    db.get('SELECT * FROM Bandalbum WHERE ID = ?', req.params.id, (err, row) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            if (!row) {
+                res.status(404).send('Bandalbum Not found');
+            } else {
+                res.json(row);
+            }
+        }
+    });
+});
+
+app.post('/Bandalbum', (req, res) => {
+    const bandalbum = req.body;
+    db.run('INSERT INTO Bandalbum (bandID, albumID) VALUES (?,?)', bandalbum.bandID, bandalbum.albumID, function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            bandalbum.ID = this.lastID;
+            res.send(bandalbum);
+        }
+    });
 });
 
 
-app.get("/books", async (req, res) => {
-    try {
-        const book = await Book.find();
-        res.send(book);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+app.put('/Bandalbum/:id', (req, res) => {
+    const bandalbum = req.body;
+    db.run('UPDATE bandalbum SET bandID = ?, albumID = ? WHERE ID = ?',  
+    bandalbum.bandID, bandalbum.albumID, req.params.id, 
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.send(bandalbum);
+            }
+        }
+    );
+});
+
+app.delete('/Bandalbum/:id', (req, res) => {
+    db.run('DELETE FROM Bandalbum WHERE ID = ?', req.params.id, function (err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send({});
+        }
+    });
 });
 
 
-app.get("/books/:id", async (req, res) => {
-    try {
-        const book = await Book.findOne( req.params.id);
-        res.send(book);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-
-app.put("/books/:id", async (req, res) => {
-    try {
-        const book = await Book.findOneAndUpdate(req.params.id, req.body,{
-            new: true,
-        });
-        res.send(book);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-
-app.delete("/books/:id", async (req, res) => {
-    try {
-        const book = await Book.findOneAndDelete(req.params.id);
-        res.send(book);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
